692acb0536b87c736ed679486a89430ab1ffbdc4
tallLeRoy <tallleroy@gmail.com>
2015-07-30 14:30:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/leroy/popularmovies_tallleroy/PostersActivityFragment.java
@@ -0,0 +1,29 @@
+package com.example.leroy.popularmovies_tallleroy;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PostersActivityFragment extends Fragment {
+
+    public PostersActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_posters, container, false);
+    }
+
+}
